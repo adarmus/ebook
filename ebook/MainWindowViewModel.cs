3014084d675989bcde1465123b0e25c1856ca60e
adarmus <adam@blairfamily.co.uk>
2014-09-11 22:04:12 +0100

    Not much

---
+++ ebook/MainWindowViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight;
+
+namespace ebook
+{
+    internal class MainWindowViewModel : ViewModelBase
+    {
+        public MainWindowViewModel()
+        {
+        }
+
+        int myVar;
+
+        public int MyProperty
+        {
+            get { return myVar; }
+            set
+            {
+                myVar = value;
+            }
+        }
+
+    }
+}

1f3b809c570da19774f55433315f835a15509699
adarmus <adam@blairfamily.co.uk>
2014-09-22 14:14:05 +0100

    Tidy up

--- ebook/MainWindow.xaml.cs
+++ ebook/MainWindow.xaml.cs
@@ -23,7 +23,7 @@ namespace ebook
     /// </summary>
     public partial class MainWindow : Window
     {
-        GridFilter _filter;
+        readonly GridFilter _filter;
 
         public MainWindow()
         {
@@ -47,77 +47,5 @@ namespace ebook
         }
     }
 
-    class StringMatcher
-    {
-    }
-
-    internal class GridFilter
-    {
-        public string AuthorFilter { get; set; }
-
-        public string TitleFilter { get; set; }
-
-        public Predicate<object> GetFilter()
-        {
-            if (string.IsNullOrWhiteSpace(AuthorFilter) && string.IsNullOrWhiteSpace(TitleFilter))
-                return null;
-
-            return BuildFilter();
-        }
-
-        public bool StringsMatch(string searchIn, string searchFor)
-        {
-            if (searchFor == null || searchIn == null)
-                return false;
-
-            return Regex.IsMatch(searchIn, string.Format(@"\b{0}", searchFor), RegexOptions.IgnoreCase);
-        }
-
-        Predicate<object> BuildFilter()
-        {
-            bool hasAuthor = !string.IsNullOrWhiteSpace(AuthorFilter);
-            bool hastitle = !string.IsNullOrWhiteSpace(TitleFilter);
-
-            if (hasAuthor && hastitle)
-            {
-                return o =>
-                {
-                    var book = o as MobiFile;
-                    
-                    if (book == null || book.Title == null || book.Author == null)
-                        return false;
-
-                    return StringsMatch(book.Title, TitleFilter) &&
-                           StringsMatch(book.Author, AuthorFilter);
-                };
-            }
-            else
-            {
-                if (hasAuthor)
-                {
-                    return o =>
-                    {
-                        var book = o as MobiFile;
-
-                        if (book == null || book.Author == null)
-                            return false;
-
-                        return StringsMatch(book.Author, AuthorFilter);
-                    };
-                }
-                else
-                {
-                    return o =>
-                    {
-                        var book = o as MobiFile;
-
-                        if (book == null || book.Title == null)
-                            return false;
-
-                        return StringsMatch(book.Title, TitleFilter);
-                    };
-                }
-            }
-        }
-    }
+    
 }

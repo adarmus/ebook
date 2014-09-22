07f0426fcda4a57c17e9f2ea125b34f8578b4648
adarmus <adam@blairfamily.co.uk>
2014-09-22 14:11:43 +0100

    Refactor

---
+++ ebook/GridFilter.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using Shell.Pdb;
+
+namespace ebook
+{
+    internal class GridFilter
+    {
+        public string AuthorFilter { get; set; }
+
+        public string TitleFilter { get; set; }
+
+        public Predicate<object> GetFilter()
+        {
+            if (string.IsNullOrWhiteSpace(AuthorFilter) && string.IsNullOrWhiteSpace(TitleFilter))
+                return null;
+
+            return BuildFilter();
+        }
+
+        public bool StringsMatch(string searchIn, string searchFor)
+        {
+            if (searchFor == null || searchIn == null)
+                return false;
+
+            return Regex.IsMatch(searchIn, string.Format(@"\b{0}", searchFor), RegexOptions.IgnoreCase);
+        }
+
+        Predicate<object> BuildFilter()
+        {
+            bool hasAuthor = !string.IsNullOrWhiteSpace(AuthorFilter);
+            bool hastitle = !string.IsNullOrWhiteSpace(TitleFilter);
+
+            if (hasAuthor && hastitle)
+            {
+                return o =>
+                {
+                    var book = o as MobiFile;
+
+                    if (book == null || book.Title == null || book.Author == null)
+                        return false;
+
+                    return StringsMatch(book.Title, TitleFilter) &&
+                           StringsMatch(book.Author, AuthorFilter);
+                };
+            }
+            else
+            {
+                if (hasAuthor)
+                {
+                    return o =>
+                    {
+                        var book = o as MobiFile;
+
+                        if (book == null || book.Author == null)
+                            return false;
+
+                        return StringsMatch(book.Author, AuthorFilter);
+                    };
+                }
+                else
+                {
+                    return o =>
+                    {
+                        var book = o as MobiFile;
+
+                        if (book == null || book.Title == null)
+                            return false;
+
+                        return StringsMatch(book.Title, TitleFilter);
+                    };
+                }
+            }
+        }
+    }
+}

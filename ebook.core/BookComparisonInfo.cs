570c8fdd48b7d786e089e104e609c04726f516be
adarmus <adam@blairfamily.co.uk>
2016-11-03 21:46:52 +0000

    Moved logic class to core

---
+++ ebook.core/BookComparisonInfo.cs
@@ -0,0 +1,11 @@
+﻿namespace ebook.core
+{
+    class BookComparisonInfo
+    {
+        public string Isbn { get; set; }
+
+        public BookInfo Book1 { get; set; }
+
+        public BookInfo Book2 { get; set; }
+    }
+}

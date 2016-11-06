626d176ef7ee0d50565b8e260fc5fcf378c50480
adarmus <adam@blairfamily.co.uk>
2016-11-06 20:18:16 +0000

    Added azure repo

---
+++ ebook.core/Repo/Azure/AzureSqlRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ebook.core.Repo.Azure
+{
+    class AzureSqlRepository
+    {
+        const string CS = "Server=tcp:ajbsql.database.windows.net,1433; Initial Catalog=books; Persist Security Info=False; User ID=ajb; Password=0TEaqG8o6daWkTwNsW1F; MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+    }
+}

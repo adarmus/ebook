302584e727bb1320ad56f93ee51d8941fc3b53d6
adarmus <adam@blairfamily.co.uk>
2016-11-07 11:05:07 +0000

    Added bits of Sql repo

---
+++ ebook.core/Repo/Sql/SqlRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ebook.core.DataTypes;
+
+namespace ebook.core.Repo.Sql
+{
+    public class SqlRepository : IBookRepository
+    {
+        public IEnumerable<BookInfo> GetBooks(bool includeMobi, bool includeEpub)
+        {
+            throw new NotImplementedException("something");   
+        }
+    }
+}

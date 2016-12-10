58a655b59f4befcfad32793f118cfc44f0fdec09
adarmus <adam@blairfamily.co.uk>
2016-12-10 16:43:28 +0000

    Stopped excluding sql folder

---
+++ ebook.core/Repo/Sql/IBookSqlDal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ebook.core.DataTypes;
+using Insight.Database;
+
+namespace ebook.core.Repo.Sql
+{
+    public interface IBookSqlDal
+    {
+        [Sql("spBook_INS")]
+        Task BookIns(BookInfo book);
+
+        [Sql("spBook_SEL_ALL")]
+        Task<IEnumerable<BookInfo>> BookSelAll();
+
+        [Sql("spBook_SEL_BY_ID")]
+        Task<BookInfo> BookSelById(Guid id);
+    }
+}
---
+++ ebook.core/Repo/Sql/SqlDataSource.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ebook.core.DataTypes;
+using Insight.Database;
+
+namespace ebook.core.Repo.Sql
+{
+    public class SqlDataSource : IFullDataSource
+    {
+        readonly SqlConnectionStringBuilder _builder;
+
+        public SqlDataSource(string connectionString)
+        {
+            _builder = new SqlConnectionStringBuilder(connectionString);
+
+            SqlInsightDbProvider.RegisterProvider();
+        }
+
+        public async Task<IEnumerable<BookInfo>> GetBooks(bool includeMobi, bool includeEpub)
+        {
+            IBookSqlDal repo = GetBookSqlDal();
+
+            try
+            {
+                var books = await repo.BookSelAll();
+
+                return books;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw;
+            }
+        }
+
+        public async Task<BookContentInfo> GetBookContent(BookInfo book)
+        {
+            try
+            {
+                IBookSqlDal repo = GetBookSqlDal();
+
+                var info = await repo.BookSelById(new Guid(book.Id));
+
+                var content = new BookContentInfo(info, "book");
+                return content;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw;
+            }
+        }
+
+        public async Task SaveBook(BookInfo book)
+        {
+            IBookSqlDal repo = GetBookSqlDal();
+
+            await repo.BookIns(book);
+        }
+
+        private IBookSqlDal GetBookSqlDal()
+        {
+            return _builder.Connection().As<IBookSqlDal>();
+        }
+    }
+}

27e9788ae47e42e34cb65f7f39551d5c2071cdd6
adarmus <adam@blairfamily.co.uk>
2016-12-11 17:16:03 +0000

    moved to new file

---
+++ ebook.core/DataTypes/MatchStatus.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ebook.core.DataTypes
+{
+    public enum MatchStatus
+    {
+        /// <summary>
+        /// The match process has not been run yet.
+        /// </summary>
+        NotAttempted,
+        /// <summary>
+        /// This is a new book.
+        /// </summary>
+        NewBook,
+        /// <summary>
+        /// This book has been matched but this contains new files.
+        /// </summary>
+        NewFiles,
+        /// <summary>
+        /// The book and all its files have been matched.
+        /// </summary>
+        UpToDate
+    }
+}

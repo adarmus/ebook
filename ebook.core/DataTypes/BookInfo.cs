1815106519b258101e1d49999a5c3a3ce81f24da
adarmus <adam@blairfamily.co.uk>
2017-02-23 09:50:58 +0000

    Added file types to grid view

--- ebook.core/DataTypes/BookInfo.cs
+++ ebook.core/DataTypes/BookInfo.cs
@@ -24,6 +24,21 @@ namespace ebook.core.DataTypes
 
         public string Title { get; set; }
 
+        public bool HasEpubFile
+        {
+            get { return HasFileWithExtension(BookExtensions.EPUB); }
+        }
+
+        public bool HasMobiFile
+        {
+            get { return HasFileWithExtension(BookExtensions.MOBI); }
+        }
+
+        bool HasFileWithExtension(string ext)
+        {
+            return this.Types.Contains(ext);
+        }
+
         public IEnumerable<string> Types { get; set; }
 
         public DateTime DateAdded { get; set; }

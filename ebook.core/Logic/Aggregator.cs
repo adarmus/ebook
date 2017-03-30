d5b6345074bcb2d7ae14cfe6db177a953adf01ad
adarmus <adam@blairfamily.co.uk>
2017-03-30 17:58:54 +0100

    Removed unused

--- ebook.core/Logic/Aggregator.cs
+++ ebook.core/Logic/Aggregator.cs
@@ -29,61 +29,14 @@ namespace ebook.core.Logic
         {
             _lookup = new Dictionary<string, BookFilesInfo>();
 
-            //IEnumerable<BookInfo> bookList = GetBookListWithIsbn(bookFiles);
-
-            //IEnumerable<BookInfo> bookListNoIsbn = GetBookListWithoutIsbn(bookFiles);
-
             IEnumerable<BookInfo> bookList = GetBookListInt(bookFiles);
 
             return bookList;
-            //return bookList.Union(bookListNoIsbn);
         }
 
         public Dictionary<string, BookFilesInfo> GetBookContentInfoLookup()
         {
             return _lookup;
-        }
-
-        IEnumerable<BookInfo> GetBookListWithIsbn(IEnumerable<BookFile> bookFiles)
-        {
-            var booksWithIsbn =
-                from m in bookFiles
-                where !string.IsNullOrEmpty(m.Isbn)
-                group m by m.Isbn
-                into g
-                select new {Isbn = g.Key, Files = g};
-
-            var bookList = booksWithIsbn.Select(b =>
-            {
-                BookFile first = b.Files.First();
-
-                var files = b.Files.Select(f => f.FilePath);
-
-                return GetBookWithIsbn(first, b.Isbn, files);
-            });
-            return bookList;
-        }
-
-        BookInfo GetBookWithIsbn(BookFile first, string isbn, IEnumerable<string> files)
-        {
-            DateTime date = _dateAdded.GetDateFromFilePath(first);
-
-            var book = new BookInfo // (files)
-            {
-                Id = Guid.NewGuid().ToString(),
-                Isbn = isbn,
-                Author = first.Author,
-                Description = first.Description,
-                PublishDate = first.PublishDate,
-                Publisher = first.Publisher,
-                Title = first.Title,
-                Types = GetFileTypes(files),
-                DateAdded = date
-            };
-
-            _lookup.Add(book.Id, new BookFilesInfo(book, files));
-
-            return book;
         }
 
         IEnumerable<BookInfo> GetBookListInt(IEnumerable<BookFile> bookFiles)
@@ -120,52 +73,6 @@ namespace ebook.core.Logic
             {
                 Id = Guid.NewGuid().ToString(),
                 Isbn = first.Isbn,
-                Author = first.Author,
-                Description = first.Description,
-                PublishDate = first.PublishDate,
-                Publisher = first.Publisher,
-                Title = first.Title,
-                Types = GetFileTypes(files),
-                DateAdded = date
-            };
-
-            _lookup.Add(book.Id, new BookFilesInfo(book, files));
-
-            return book;
-        }
-
-        IEnumerable<BookInfo> GetBookListWithoutIsbn(IEnumerable<BookFile> bookFiles)
-        {
-            var booksNoIsbn =
-                from m in bookFiles
-                where string.IsNullOrEmpty(m.Isbn)
-                group m by new
-                {
-                    m.Title,
-                    m.Author
-                }
-                into g
-                select new { Title = g.Key.Title, Author = g.Key.Author, Files = g };
-
-            var bookListNoIsbn = booksNoIsbn.Select(b =>
-            {
-                BookFile first = b.Files.First();
-
-                var files = b.Files.Select(f => f.FilePath);
-
-                return GetBookWithoutIsbn(first, files);
-            });
-            return bookListNoIsbn;
-        }
-
-        BookInfo GetBookWithoutIsbn(BookFile first, IEnumerable<string> files)
-        {
-            DateTime date = _dateAdded.GetDateFromFilePath(first);
-
-            var book = new BookInfo // (first.FilePath)
-            {
-                Id = Guid.NewGuid().ToString(),
-                Isbn = "",
                 Author = first.Author,
                 Description = first.Description,
                 PublishDate = first.PublishDate,
